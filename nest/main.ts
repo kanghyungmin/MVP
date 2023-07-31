2cc41ffb554c65668548928bf970900170ee7fd8
hyungmin <hmkkang0922@daum.net>
2023-07-31 18:30:07 +0900

    connecting db

--- nest/main.ts
+++ nest/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from "@/nest/app.module";
 import * as http from "http";
 import { NextApiHandler } from "next";
 import { INestApplication } from "@nestjs/common";
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 
 export module Main {
   let app: INestApplication;
@@ -11,6 +12,7 @@ export module Main {
     if (!app) {
       app = await NestFactory.create(AppModule, { bodyParser: false });
       app.setGlobalPrefix("api");
+      setupSwagger(app);
       await app.init();
     }
 
@@ -19,9 +21,22 @@ export module Main {
 
   export async function getListener() {
     const app = await getApp();
+
     const server: http.Server = app.getHttpServer();
+
     const [listener] = server.listeners("request") as NextApiHandler[];
 
     return listener;
+  }
+
+  function setupSwagger(app: INestApplication): void {
+    const options = new DocumentBuilder()
+      .setTitle("NestJS API Docs")
+      .setDescription("NestJS API description")
+      .setVersion("1.0.0")
+      .build();
+
+    const document = SwaggerModule.createDocument(app, options);
+    SwaggerModule.setup("api-docs", app, document);
   }
 }
