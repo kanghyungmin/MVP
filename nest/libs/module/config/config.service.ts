5195122eb8eb0e3ccc776b6e06b4f5dbe63ea391
hyungmin <hmkkang0922@daum.net>
2023-07-31 15:39:31 +0900

    add db connection module

---
+++ nest/libs/module/config/config.service.ts
@@ -0,0 +1,11 @@
+import * as dotenv from "dotenv";
+import { Injectable, Inject } from "@nestjs/common";
+
+@Injectable()
+export class ConfigService {
+  //   private readonly envConfig: EnvConfig;
+
+  constructor(@Inject("CONFIG_OPTIONS") private options: Record<string, any>) {
+    dotenv.config({ path: options.envPath });
+  }
+}
