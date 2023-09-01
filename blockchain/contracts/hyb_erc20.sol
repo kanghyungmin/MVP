2d9daff73145a07bd6a92bf844e856d7b3e81e9b
hyungmin <hmkkang0922@daum.net>
2023-09-01 15:51:11 +0900

    mint & project structure

--- blockchain/contracts/hyb_erc20.sol
+++ blockchain/contracts/hyb_erc20.sol
@@ -6,5 +6,5 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 contract MyToken is ERC20 {
     constructor() ERC20("MyToken", "MTK") {
         _mint(msg.sender, 1 * 10 ** decimals());
-    }
-}
+    }                                                   
+}                       
