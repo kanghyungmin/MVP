1a583e12a6cb5dcd755dc1a2ae58ed7d638310df
hyungmin <hmkkang0922@daum.net>
2023-09-04 11:17:05 +0900

    complete setting projects

--- blockchain/contracts/hyb_erc20.sol
+++ blockchain/contracts/hyb_erc20.sol
@@ -2,9 +2,14 @@
 pragma solidity ^0.8.13;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MyToken is ERC20 {
-    constructor() ERC20("MyToken", "MTK") {
-        _mint(msg.sender, 1 * 10 ** decimals());
-    }                                                   
+contract MyToken is ERC20, ERC20Burnable, Ownable {
+    constructor() ERC20("MyToken", "MTK") {}
+    
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+                                                       
 }                       
--- blockchain/scripts/deploy.ts
+++ blockchain/scripts/deploy.ts
@@ -8,6 +8,10 @@ async function main() {
   await erc20.deployed();
 
   console.log(`erc20 contract is deployed to ${erc20.address}`);
+  console.log("deploying MyERC721 contract");
+  console.log(
+    `${await erc20.balanceOf("0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266")}`
+  );
 }
 
 // We recommend this pattern to be able to use async/await everywhere
