7f26723c3bfb5a691f9aa3bf99102dfabecc8e3a
hyungmin <hmkkang0922@daum.net>
2023-09-14 18:13:52 +0900

    add script files for erc721A

---
+++ blockchain/scripts/ERC721A/mint.ts
@@ -0,0 +1,23 @@
+import { ethers } from "hardhat";
+import { ethers as ethersType } from "ethers";
+
+const contractAddress = process.env.ERC721A!;
+const price = 0.001;
+
+async function mint(amount: number) {
+  console.log("mint from ERC721A contract");
+  const Erc721A = await ethers.getContractFactory("MyERC721A");
+  const erc721A = await Erc721A.attach(contractAddress);
+  const mint = await erc721A.mint(amount, {
+    value: ethersType.utils.parseEther((price * amount).toString()),
+  });
+  console.log("mint :", mint);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+const amount = 3;
+mint(amount).catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
