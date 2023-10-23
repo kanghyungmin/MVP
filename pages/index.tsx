d151878b8ffb9199cee8959746610d687dbcea70
hyungmin <hmkkang0922@daum.net>
2023-10-23 19:17:03 +0900

    make xm styly responsively

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,13 +3,10 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  // return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
   return (
     <header>
       {/* NavBar */}
       <nav>
-        {/* <div className="w-[375px] h-[60px] bg-neutral-950 flex justify-start items-center"> */}
-
         <div className="w-full min-h-[60px] md:h-[80px] bg-neutral-950 flex justify-between items-center">
           <div className="ml-[15px] relative justify-start items-start flex">
             <img
@@ -29,13 +26,7 @@ export default function Home() {
         </div>
       </nav>
       {/* Hero Image-SM */}
-      <div className="sm:hidden w-full h-[600px] relative bg-neutral-950">
-        <div className="w-full pb-[1px] left-0 top-[300px] absolute justify-center items-center inline-flex z-10">
-          <img
-            className="origin-top-left w-full z-10"
-            src="/img-background-landing_M.png"
-          />
-        </div>
+      <div className="sm:hidden w-full h-[541px] relative bg-neutral-950">
         <div className="grid grid-cols-1 w-full h-[272px] justify-center gap-10 z-20">
           <div className="h-[272px] flex-col justify-center justify-items-start gap-5 flex mt-[100px] ">
             <div className="text-center text-white text-5xl font-bold font-['DM Sans'] leading-[60px]">
@@ -44,7 +35,7 @@ export default function Home() {
               <br />
               모든 경험!
             </div>
-            <div className="text-center text-gray-200 text-base font-medium font-['Pretendard'] leading-normal z-10">
+            <div className="text-center text-gray-200 text-base font-medium font-['Pretendard'] leading-normal z-20">
               응원하는 가수의 공연 티켓 예매부터, 투표
               <br />
               그리고 함께 이야기를 나눌 수 있는
@@ -53,14 +44,23 @@ export default function Home() {
             </div>
           </div>
         </div>
-        <div className="ml-[24px] mt-[-205px] z-30 relative">
-          <div className="w-[22px] h-[22px] bg-gradient-to-br from-blue-200 to-violet-400 rounded-full shadow">
-            <img className="origin-top-left w-full" src="/img-dot-purple.png" />
-            <div className="ml-[307px] mt-[304px] w-[20px] h-[20px] bg-pink-300 rounded-full shadow"></div>
-          </div>
-          <div className="ml-[143px] mt-[408px] w-[20px] h-[20px] absolute origin-top-left rotate-[18.62deg] bg-gradient-to-br from-amber-100 to-orange-400 rounded-full shadow" />
+        <div className="w-full pb-[1px] bg-neutral-500 relative justify-center items-center inline-flex z-10">
+          <img
+            className="origin-top-left w-full z-10"
+            src="/img-background-landing_M.png"
+          />
+        </div>
+        <div className="flex absolute mt-[-88.73px] w-full z-10 justify-end">
+          <div className="relative mr-[176.89px] w-[20px] h-[20px]  origin-top-left rotate-[0deg] bg-gradient-to-br from-amber-100 to-orange-400 rounded-full shadow z-10" />
+        </div>
+        <div className="flex absolute w-full mt-[-193px] z-10 justify-end">
+          <div className="w-[20px] mr-[14px] h-[20px] bg-pink-300 rounded-full shadow" />
+        </div>
+        <div className="flex absolute w-full mt-[-505px] z-10 justify-start">
+          <div className="w-[22px] ml-[14px] h-[22px] bg-gradient-to-br from-blue-200 to-violet-400 rounded-full shadow" />
         </div>
       </div>
+
       {/* Hero Image-MD */}
       <div className="sm:block hidden w-full h-[880px] relative bg-neutral-950">
         <div className="w-[768px] pt-[5px] pb-[35px] left-0 top-[488px] absolute justify-center items-center inline-flex">
